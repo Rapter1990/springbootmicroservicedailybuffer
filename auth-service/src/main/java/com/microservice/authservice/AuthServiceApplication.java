e0cd3910400be3be7ab759d1610437546b2f966a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-02-13 16:53:32 +0300

    Task 81 : Add EnableFeignClients annotation for Auth Service, Payment Service and Product Service

--- auth-service/src/main/java/com/microservice/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/microservice/authservice/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
--- paymentservice/src/main/java/com/microservice/paymentservice/PaymentserviceApplication.java
+++ paymentservice/src/main/java/com/microservice/paymentservice/PaymentserviceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentserviceApplication {
 
 	public static void main(String[] args) {
