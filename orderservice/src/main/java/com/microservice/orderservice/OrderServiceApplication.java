1a0420a45a15a1fd442894e24232f8a916dd1cdd
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-02-13 16:58:32 +0300

    Task 82 : Add EnableFeignClients annotation for Order Service

--- orderservice/src/main/java/com/microservice/orderservice/OrderServiceApplication.java
+++ orderservice/src/main/java/com/microservice/orderservice/OrderServiceApplication.java
@@ -3,12 +3,14 @@ package com.microservice.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
